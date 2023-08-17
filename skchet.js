f092c84e27abd15798462483e801afcc34dd0c72
brunokas <141346052+brunokas@users.noreply.github.com>
2023-08-17 13:55:12 -0300

    adicionando arquivos js e css

---
+++ skchet.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(400, 400);
+   background(255);
+  }
+  function draw() {
+    
+    rect(mouseX,mouseY,random(20,30),random(20,30));
+    fill(random(0,255),random(0,255),random(0,255));
+    
+   
